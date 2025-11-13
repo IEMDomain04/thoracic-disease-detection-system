459d8627f3c8baaac9a54fd41e2c134d4ee45176
IEMDomain04 <emmanman0011@gmail.com>
2025-11-13 16:35:55 +0800

    updated header changed color; TODO: Adjust color for the upload image section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,28 @@
 import { Card } from './ui/card';
 import { Activity } from 'lucide-react';
 
+// Parang maganda rin itong color na to. bg-gradient-to-b from-[#374151] to-[#1F2937]
+
 export function Header() {
   return (
-    <Card className="rounded-none border-x-0 border-t-0 shadow-md">
-      <div className="container mx-auto px-4 py-6">
+      <div className="border px-20 py-10 bg-gradient-to-b from-[#0EA5E9] via-[#1F2937] to-[#111827]">
         <div className="flex items-center gap-3">
-          <div className="p-2 bg-blue-100 rounded-lg">
-            <Activity className="h-6 w-6 text-blue-600" />
+
+          {/* Icon */}
+          <div className="p-2 bg-[#0EA5E9]/10 rounded-lg">
+            <Activity className="h-6 w-6 text-[#38BDF8]" />
           </div>
+
+          {/* Header: Title and Subtitle */}
           <div>
-            <h1 className="text-gray-900">X-ray Classification System</h1>
-            <p className="text-sm text-gray-600 mt-1">
+            <h1 className="text-[#F9FAFB] font-semibold">
+              X-ray Classification System
+            </h1>
+            <p className="text-sm text-[#9CA3AF] mt-1">
               Upload, classify, and visualize medical images with Grad-CAM
             </p>
           </div>
         </div>
       </div>
-    </Card>
   );
 }
